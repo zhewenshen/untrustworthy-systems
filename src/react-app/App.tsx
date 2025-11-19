584c9a6d575c85bc945f8c571b56d5a4ea6ed596
zhewen shen <zhewen.shen@outlook.com>
2025-11-19 18:55:00 +1100

    test page

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-import CalculatorPage from "./pages/CalculatorPage.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import "./App.css";
 
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/react-app/pages/Home.tsx
+++ src/react-app/pages/Home.tsx
@@ -1,16 +1,7 @@
-import { Link } from "react-router-dom";
-
 function Home() {
   return (
     <div>
       <h1>HELLO WORLD</h1>
-      <div style={{ marginTop: "20px" }}>
-        <Link to="/calculator">
-          <button style={{ padding: "10px 20px", fontSize: "16px" }}>
-            Calculator
-          </button>
-        </Link>
-      </div>
     </div>
   );
 }
