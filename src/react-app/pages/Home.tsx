3c276d59319fb40880ac3dedbdd29b0adc2a485e
zhewen shen <zhewen.shen@outlook.com>
2025-11-19 18:58:21 +1100

    test page

--- src/react-app/pages/Home.tsx
+++ src/react-app/pages/Home.tsx
@@ -1,7 +1,24 @@
 function Home() {
   return (
-    <div>
-      <h1>HELLO WORLD</h1>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh"
+    }}>
+      <a href="https://trustworthy.systems/" target="_blank">
+        <button style={{
+          padding: "40px 80px",
+          fontSize: "48px",
+          cursor: "pointer",
+          backgroundColor: "#4CAF50",
+          color: "white",
+          border: "none",
+          borderRadius: "10px"
+        }}>
+          Go to Trustworthy Systems
+        </button>
+      </a>
     </div>
   );
 }
