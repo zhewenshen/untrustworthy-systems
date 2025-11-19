372dc47611badd1025159cb0b8ebbb149eace2db
Zhewen Shen <67468885+zhewenshen@users.noreply.github.com>
2025-11-19 18:22:27 +1100

    test

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -31,7 +31,7 @@ function App() {
           />
         </a>
       </div>
-      <h1>Vite + React + Hono + Cloudflare</h1>
+      <h1>Vite + React + Hono + Cloudflare. HELLO WORLD TEST</h1>
       <div className="card">
         <button
           onClick={() => setCount((count) => count + 1)}
